1279f4d5b0472dbcb01bce01f315735e35a045a7
Angel Christov <christovangel@gmail.com>
2023-04-19 18:16:47 -0700

    Login Page, Landing Page, Registration Page, created. Still need further revision

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,10 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
+// GET /users/[userID]
+// CRUD - Create, UPdate, Delete
+// API Methods -  GET, PATCH, PUT, DELETE
+// /users - GET, PATCH, DELETE
+// localhost:8080/users/[userID]
