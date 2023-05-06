6770b8c4d8fff08f4560ff094b6b9efc041282c9
Greg J <39595823+OfficialGregJ@users.noreply.github.com>
2023-05-05 17:01:46 -0700

    Implemented DB query into API
    - Created and verified GET method for API
    - Created basic template that can be used anywhere to access user information from the API (stored in index.js)
    - NavBar still bugged? When attempting to create new branch, pulls old NavBar rather than new one

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Login from "./login";
 import { useRouter } from "next/router";
 import { useUser } from '@supabase/auth-helpers-react'
+import React from "react";
 
 export default function Index() {
     const router = useRouter();
@@ -13,3 +14,26 @@ export default function Index() {
     }
 
 }
+
+// Any page that will fetch data for the user should start with this code.
+
+// const [data, setData] = useState(null);
+//
+// if(useUser() == null) {
+//     return <Index/>;
+// }
+//
+// // Fetch the user data, can be copied to each page that accesses user data
+// // Adding [user] to the end doesn't cause null error, forces to wait for hook to be used or updated?
+// const fetchData = async () => {
+//     let user = useUser().id;
+//     let url = "/api/user/?id=" + user;
+//     let userData = await fetch(url)
+//     return userData.json();
+// };
+//
+// fetchData().then(result => {
+//     setData(result[0]);
+// }).catch(error => {
+//     console.error(error);
+// });
